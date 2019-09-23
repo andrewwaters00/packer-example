62c3820f8d74c723f05229e402c2f858cd24e78a
andrewwaters00 <andrew.waters00@googlemail.com>
2019-09-23 10:03:54 +0100

    Uploading packer files to repo

---
+++ example.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+echo "Updating packages - wait for completion"
+
+sudo apt update -y
+
+echo "Install python3 and boto3"
+
+sudo apt-get install python3 -y
+
+echo "Show python is installed and version"
+
+python3 --version
+
+echo "Install zip"
+
+sudo apt-get install zip -y
+
+echo "Installing terraform version 0.12.9"
+
+curl https://releases.hashicorp.com/terraform/0.12.9/terraform_0.12.9_linux_amd64.zip --output terraform_0.12.9.zip && unzip terraform_0.12.9.zip && sudo mv terraform /usr/local/sbin
+
+echo "Checking terraform version installed"
+
+terraform --version
+
+echo "Installing docker-ce"
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+echo "Updating docker-ce from community repo"
+
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+echo "Verify docker is running"
+
+sudo docker run hello-world
+
+echo "Pull latest docker image for traefik"
+
+sudo docker pull traefik:latest
+
+
